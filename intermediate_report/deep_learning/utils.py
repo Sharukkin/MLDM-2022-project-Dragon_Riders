209997078708c402f002e886c652297371f47cb2
Mikhail <91428782+Sharukkin@users.noreply.github.com>
2022-12-03 21:51:05 +0300

    Add files via upload

---
+++ intermediate_report/deep_learning/utils.py
@@ -0,0 +1,62 @@
+# Definition of utils functions
+import os
+import time
+import math
+import random
+
+import numpy as np
+from sklearn.metrics import mean_squared_error
+import torch
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+OUTPUT_DIR = './'
+
+def MCRMSE(y_trues, y_preds):
+    scores = []
+    idxes = y_trues.shape[1]
+    for i in range(idxes):
+        y_true = y_trues[:,i]
+        y_pred = y_preds[:,i]
+        score = mean_squared_error(y_true, y_pred, squared=False) # RMSE
+        scores.append(score)
+    mcrmse_score = np.mean(scores)
+    return mcrmse_score, scores
+
+
+def get_score(y_trues, y_preds):
+    mcrmse_score, scores = MCRMSE(y_trues, y_preds)
+    return mcrmse_score, scores
+
+
+def get_logger(filename=OUTPUT_DIR+'train'):
+    from logging import getLogger, INFO, StreamHandler, FileHandler, Formatter
+    logger = getLogger(__name__)
+    logger.setLevel(INFO)
+    handler1 = StreamHandler()
+    handler1.setFormatter(Formatter("%(message)s"))
+    handler2 = FileHandler(filename=f"{filename}.log")
+    handler2.setFormatter(Formatter("%(message)s"))
+    logger.addHandler(handler1)
+    logger.addHandler(handler2)
+    return logger
+
+def seed_everything(seed=2807):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+def asMinutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+
+def timeSince(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (remain %s)' % (asMinutes(s), asMinutes(rs))
